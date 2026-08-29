faac95fe6f8766c5f01343caddbc571cc1cf6e10
Shamsudeenmohammed <mshamsudeen551@gmail.com>
2026-08-29 07:16:27 +0000

    url reconfigured

--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('stophack/', admin.site.urls),
     path('', include('apps.dashboard.urls')),
     path('accounts/', include('apps.accounts.urls')),
     path('customers/', include('apps.customers.urls')),
